ded0c7831caf8172377448e8f61d5676d19ab2e9
刘立俊 <645604899@qq.com>
2020-07-25 11:04:06 +0800

    Initial commit

---
+++ ftl-test/src/main/java/com/cloud/ftl/ftltest/FtlTestApplication.java
@@ -0,0 +1,19 @@
+package com.cloud.ftl.ftltest;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+@ComponentScan("com.cloud")
+@MapperScan("com.cloud.**.dao")
+public class FtlTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtlTestApplication.class, args);
+    }
+
+}
